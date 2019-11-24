a1341caef107d6134415373970b1384d56d63d9b
Markus Faßbender <markus.fassbender@posteo.de>
2019-11-24 15:57:14 +0100

    remove init nullability from external ios user agent

--- Source/iOS/OIDExternalUserAgentIOS.h
+++ Source/iOS/OIDExternalUserAgentIOS.h
@@ -30,7 +30,7 @@ NS_ASSUME_NONNULL_BEGIN
 API_UNAVAILABLE(macCatalyst)
 @interface OIDExternalUserAgentIOS : NSObject<OIDExternalUserAgent>
 
-- (nullable instancetype)init API_AVAILABLE(ios(11))
+- (instancetype)init API_AVAILABLE(ios(11))
     __deprecated_msg("This method will not work on iOS 13, use "
                      "initWithPresentingViewController:presentingViewController");
 
@@ -38,7 +38,7 @@ API_UNAVAILABLE(macCatalyst)
     @param presentingViewController The view controller from which to present the
         \SFSafariViewController.
  */
-- (nullable instancetype)initWithPresentingViewController:
+- (instancetype)initWithPresentingViewController:
     (UIViewController *)presentingViewController
     NS_DESIGNATED_INITIALIZER;
 
